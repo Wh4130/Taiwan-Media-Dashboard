08b15ba2fe8662a5478dfb5d33b079cd7fb31a66
Huang Lin Chun <huang0jin@gmail.com>
2025-11-14 23:10:34 +0800

    changed param k

--- utils/constants.py
+++ utils/constants.py
@@ -14,12 +14,12 @@ media_sources = {
 SCRAPER_SETTINGS = {
     "cna": {
         "base_url": "https://www.cna.com.tw/list/aall.aspx",
-        "K": 2,
+        "K": 30,
         "T": 0.1
     },
     "udn": {
         "base_url": "https://udn.com/api/more",
-        "K": 2,
+        "K": 30,
         "T": 0.1
     }
 }
